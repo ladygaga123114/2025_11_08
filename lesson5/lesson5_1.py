ae1f41a089993e76516f3284b92f719e3d96ddbc
roberthsu2003 <roberthsu2003@gmail.com>
2025-11-29 13:35:39 +0800

    5-1.2--2次提交

--- lesson5/lesson5_1.py
+++ lesson5/lesson5_1.py
@@ -1,16 +1,14 @@
 import os
 from playwright.sync_api import sync_playwright
 
-def get_html_path(filename: str) -> str:
+def get_html_path()-> str:
     """返回HTML文件的絕對路徑"""
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    html_path = os.path.join(current_dir, filename)
+    html_path = os.path.join(current_dir, "waiting_demo.html")
     return f"file://{html_path}"
 
 def main():
-    # 這裡指定要開啟的 HTML 檔案名稱
-    html_filename = "waiting_demo.html"  # 你可以改成其他檔名
-    path = get_html_path(html_filename)
+    path = get_html_path()
     with sync_playwright() as p:
         # 啟動瀏覽器
         browser = p.chromium.launch(headless=False, slow_mo=500)
@@ -20,7 +18,13 @@ def main():
 
         page.goto(path)
 
+        
+        page.wait_for_load_state("networkidle")  # 等待網絡空閒
+        #page.click("#trigger-delayed")  # 點擊按鈕觸發異步操作
+        delay_button = page.locator("#trigger-delayed")  #取後Locator按鈕觸
+        delay_button.click()  #發送點擊事件
         page.wait_for_timeout(3000)  # 等待3秒以觀察效果
+
 
         browser.close()
 
